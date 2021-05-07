1c2332847e662d06733b45fa29318893d824e16c
Imaka <79092624+Imangali-ang@users.noreply.github.com>
2021-05-07 14:52:47 +0500

    site

---
+++ louis veton/funct.js
@@ -0,0 +1,20 @@
+function Fun1() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("carouselExampleControls").classList.toggle("disapeer");
+  }
+
+  function Fun2() {
+    document.getElementById("hz1").classList.toggle("showwom");
+  }
+
+  function women(){
+    document.getElementById("hz2").style.display="none";
+    document.getElementById("nowwomencont").classList.toggle("showwom");
+  }
+
+  function man(){
+    document.getElementById("nowwomencont").classList.remove("showwom");
+    document.getElementById("hz2").style.display="block";
+  }
+  
+ 
